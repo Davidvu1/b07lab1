f43b1ab6efd7e5517218d345c86741071ee1eebc
Davidvu1 <72768452+Davidvu1@users.noreply.github.com>
2025-09-21 18:32:49 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,8 +1,46 @@
 
+
 public class Polynomial {
 
-    double coefficients[]
+    private double[] coefficients;
 
+    public Polynomial() {
+
+        this.coefficients = new double[1];
+    }
+
+    public Polynomial(double[] coefficients) {
+
+        this.coefficients = coefficients;
+
+    }
+
+    public Polynomial add(Polynomial p) {
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            p.coefficients[i]= this.coefficients[i] + p.coefficients[i];
+        }
+
+        return p;
+    }
+
+    public double evaluate(double x) {
+
+        double total = 0.0;
+
+        for (int i = 0; i < this.coefficients.length; i++ ) {
+
+            total += this.coefficients[i] * Math.pow(x, i);
+        }
+
+        return total;
+
+    }
+
+    public boolean hasRoot(double x) {
+
+        return this.evaluate(x) == 0;
+    }
 
 
 }
